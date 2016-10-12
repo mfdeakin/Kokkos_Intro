43b261ca8b0d89f399701cf5361a97c2cd1abe0d
Michael Deakin <mfdeakin@cs.unc.edu>
2016-10-11 23:15:04 -0600

    Move Kokkos configuration options to CMakeLists.txt

--- include/KokkosCore_config.h
+++ include/KokkosCore_config.h
@@ -1,10 +1,4 @@
-/* ---------------------------------------------
-Makefile constructed configuration:
-Wed Sep 21 20:35:55 EDT 2016
-----------------------------------------------*/
 /* Execution Spaces */
-#define KOKKOS_HAVE_PTHREAD 1
 /* General Settings */
-#define KOKKOS_HAVE_CXX11 1
 /* Optimization Settings */
 /* Cuda Settings */
